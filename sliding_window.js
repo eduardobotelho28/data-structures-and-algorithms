95054c2796550800fe08f1e8fac668dac0eeaf4f
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2025-02-03 15:23:51 -0300

    sliding window

---
+++ sliding_window.js
@@ -0,0 +1,32 @@
+function sliding_window (string) {
+
+    let l = 0,  r = 0
+    let max  = 1
+    const counter = {}
+
+    counter[string[0]] = 1
+
+    while (r < string.length - 1) {
+        r+=1
+
+        if(counter[string[r]])  {
+            counter[string[r]] += 1
+        }
+
+        else {
+            counter[string[r]] = 1
+        }
+
+        while (counter[string[r]] === 3) {
+            counter[string[l]] -=1
+            l+= 1
+        }
+
+        max = max > r-l+1 ? max : r-l+1
+    }
+
+    return max
+
+}
+
+sliding_window("teste")
